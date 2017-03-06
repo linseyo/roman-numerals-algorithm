c2c7efc41b3bda148e217c9aff2857d8cb8353e1
Dan McKeon & Nick Jarin <pair+dpmckeon1.njarin@devbootcamp.com>
2017-03-06 15:42:39 -0800

    refactor hash and make code DRY

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,17 +1,34 @@
 def convert_to_roman(arabic_number, modern = false)
   roman_numeral = ""
   running_total = arabic_number
-  roman_numeral_pairs = {
-    1000 => "M",
-    500 => "D",
-    100 => "C",
-    50 => "L",
-    10 => "X",
-    5 => "V",
-    1 => "I"
-  }
 
-  roman_numeral_order = [1000, 500, 100, 50, 10, 5, 1]
+      roman_numeral_pairs = {
+      1000 => "M",
+      500 => "D",
+      100 => "C",
+      50 => "L",
+      10 => "X",
+      5 => "V",
+      1 => "I"
+    }
+
+  if modern
+    roman_numeral_pairs_modern = {
+      900 => "CM",
+      400 => "CD",
+      90 => "XC",
+      40 => "XL",
+      9 => "IX",
+      4 => "IV"
+    }
+
+    roman_numeral_pairs.merge!(roman_numeral_pairs_modern)
+
+    roman_numeral_order = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+
+  else
+    roman_numeral_order = [1000, 500, 100, 50, 10, 5, 1]
+  end
 
   roman_numeral_order.each do |roman_key|
     symbols_needed = (running_total/roman_key).floor
@@ -19,26 +36,5 @@ def convert_to_roman(arabic_number, modern = false)
     running_total -= symbols_needed * roman_key
   end
 
-  if modern
-    roman_array = roman_numeral.split('')
-    i = 0
-    until i == roman_array.length
-      if (roman_array[i] == roman_array[i+1]) && (roman_array[i] == roman_array[i+2]) && (roman_array[i] == roman_array[i+3])
-        puts "It's here!"
-      end
-      i += 1
-    end
-  end
-
   roman_numeral
 end
-
-convert_to_roman(4, true) #=> "IIII" but it should be "IV"
-convert_to_roman(9, true) #=> "VIIII" but it should be "IX"
-convert_to_roman(14, true) #=> "XIIII" but it should be "XIV"
-convert_to_roman(44, true) #=> "XXXXIIII" but it should be "XLIV"
-convert_to_roman(99, true) #=> "LXXXXVIIII" but it should be "XCIX"
-convert_to_roman(400, true) #=> "CCCC" but it should be "CD"
-convert_to_roman(944, true) #=> "DCCCCXXXXIIII" but it should be "CMXLIV"
-
-# [900, 400, 90, 40, 9, 4]
