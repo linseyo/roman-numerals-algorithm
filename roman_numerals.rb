29c2623d89cdc6abd026cecce122a24bf4008fd0
Jordan Levine & Julia (Julie) Xu <pair+j0rdania.jmxu94@devbootcamp.com>
2017-03-06 15:48:08 -0800

    Write convert_to_roman method for V feature

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,7 +1,17 @@
+
+# converts arabic numbers to old roman for V and I
 def convert_to_roman(arabic_number)
   roman_number = ""
-  arabic_number.times {
+
+  v_quantity = arabic_number/5
+  v_quantity.times {
+    roman_number += "V"
+  }
+
+  i_quantity = arabic_number%5
+  i_quantity.times {
     roman_number += "I"
   }
+
   roman_number
 end
