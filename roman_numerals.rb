b7a82a344a7db59e48650351cdc3ba39efa93cc5
Dan McKeon & Nick Jarin <pair+dpmckeon1.njarin@devbootcamp.com>
2017-03-06 12:39:54 -0800

    add access to repetitive numerals

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,4 +1,4 @@
-def convert_to_roman(arabic_number)
+def convert_to_roman(arabic_number, modern = false)
   roman_numeral = ""
   running_total = arabic_number
   roman_numeral_pairs = {
@@ -19,5 +19,26 @@ def convert_to_roman(arabic_number)
     running_total -= symbols_needed * roman_key
   end
 
+  if modern
+    roman_array = roman_numeral.split('')
+    i = 0
+    until i == roman_array.length
+      if (roman_array[i] == roman_array[i+1]) && (roman_array[i] == roman_array[i+2]) && (roman_array[i] == roman_array[i+3])
+        puts "It's here!"
+      end
+      i += 1
+    end
+  end
+
   roman_numeral
 end
+
+convert_to_roman(4, true) #=> "IIII" but it should be "IV"
+convert_to_roman(9, true) #=> "VIIII" but it should be "IX"
+convert_to_roman(14, true) #=> "XIIII" but it should be "XIV"
+convert_to_roman(44, true) #=> "XXXXIIII" but it should be "XLIV"
+convert_to_roman(99, true) #=> "LXXXXVIIII" but it should be "XCIX"
+convert_to_roman(400, true) #=> "CCCC" but it should be "CD"
+convert_to_roman(944, true) #=> "DCCCCXXXXIIII" but it should be "CMXLIV"
+
+# [900, 400, 90, 40, 9, 4]
