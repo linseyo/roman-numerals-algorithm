a2f3240dfa06a10ddf78e313e65f6be458e7dc2f
Eitan Greene & Matthew Fischer <pair+eitan-g.mattgfisch@devbootcamp.com>
2017-03-06 11:46:28 -0800

    Refactors to loop through letters and numbers arrays

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,33 +1,13 @@
 def convert_to_roman(arabic_number)
-  letters = ["M", "D", "C", "L"]
+  letters = ["M", "D", "C", "L", "X", "V", "I"]
+  numbers = [1000, 500, 100, 50, 10, 5, 1]
+
   response = ""
-  if arabic_number >= 1000
-    response += "M" * (arabic_number / 1000)
-    arabic_number = arabic_number % 1000
-  end
-  if arabic_number >= 500
-    response += "D" * (arabic_number / 500)
-    arabic_number = arabic_number % 500
-  end
-  if arabic_number >= 100
-    response += "C" * (arabic_number / 100)
-    arabic_number = arabic_number % 100
-  end
-  if arabic_number >= 50
-    response += "L" * (arabic_number / 50)
-    arabic_number = arabic_number % 50
-  end
-  if arabic_number >= 10
-    response += "X" * (arabic_number / 10)
-    arabic_number = arabic_number % 10
-  end
-  if arabic_number >= 5
-    response += "V" * (arabic_number / 5)
-    arabic_number = arabic_number % 5
-  end
-  if arabic_number >= 1
-    response += "I" * (arabic_number / 1)
-    arabic_number = arabic_number % 1
+  numbers.each do |num|
+    if arabic_number >= num
+      response += letters[numbers.index(num)] * (arabic_number / num)
+      arabic_number = arabic_number % num
+    end
   end
 
   response
