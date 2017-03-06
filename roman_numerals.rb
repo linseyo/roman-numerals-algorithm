6599069bdd456371b3ed0c552759953d7fa903b9
Dan McKeon & Nick Jarin <pair+dpmckeon1.njarin@devbootcamp.com>
2017-03-06 11:56:37 -0800

    refactor roman converter to iteration model

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,34 +1,23 @@
 def convert_to_roman(arabic_number)
   roman_numeral = ""
   running_total = arabic_number
+  roman_numeral_pairs = {
+    1000 => "M",
+    500 => "D",
+    100 => "C",
+    50 => "L",
+    10 => "X",
+    5 => "V",
+    1 => "I"
+  }
 
-  num_thousands = (running_total/1000).floor
-  num_thousands.times {roman_numeral += "M"}
-  running_total -= num_thousands * 1000
+  roman_numeral_order = [1000, 500, 100, 50, 10, 5, 1]
 
-  num_five_hundreds = (running_total/500).floor
-  num_five_hundreds.times {roman_numeral += "D"}
-  running_total -= num_five_hundreds * 500
-
-  num_hundreds = (running_total/100).floor
-  num_hundreds.times {roman_numeral += "C"}
-  running_total -= num_hundreds * 100
-
-  num_fifties = (running_total/50).floor
-  num_fifties.times {roman_numeral += "L"}
-  running_total -= num_fifties * 50
-
-  num_tens = (running_total/10).floor
-  num_tens.times {roman_numeral += "X"}
-  running_total -= num_tens * 10
-
-  num_fives = (running_total/5).floor
-  num_fives.times {roman_numeral += "V"}
-  running_total -= num_fives * 5
-
-  num_ones = (running_total%5)
-  num_ones.times {roman_numeral += "I"}
-
+  roman_numeral_order.each do |roman_key|
+    symbols_needed = (running_total/roman_key).floor
+    symbols_needed.times {roman_numeral += roman_numeral_pairs[roman_key]}
+    running_total -= symbols_needed * roman_key
+  end
 
   roman_numeral
 end
