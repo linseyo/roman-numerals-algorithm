3e7f27fb238ac8be0c50792ed4424b2c3a230338
Dan McKeon & Nick Jarin <pair+dpmckeon1.njarin@devbootcamp.com>
2017-03-06 11:29:05 -0800

    add tests, add 5 and 10 logic

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,5 +1,23 @@
 def convert_to_roman(arabic_number)
   roman_numeral = ""
-  arabic_number.times {roman_numeral += "I"}
+  running_total = arabic_number
+
+  # num_fifties = (arabic_number/10).floor
+  # num_tens.times {roman_numeral += "X"}
+  # running_total -= num_tens * 10
+
+
+  num_tens = (arabic_number/10).floor
+  num_tens.times {roman_numeral += "X"}
+  running_total -= num_tens * 10
+
+  num_fives = (running_total/5).floor
+  num_fives.times {roman_numeral += "V"}
+  running_total -= num_fives * 5
+
+  num_ones = (running_total%5)
+  num_ones.times {roman_numeral += "I"}
+
+
   roman_numeral
 end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -12,7 +12,39 @@ describe 'converting an Arabic number to a Roman numeral' do
 
     # Release 1 ...
     # add tests for old roman numerals here
+    # Write tests for 5, 9, 10, 55, 101, 600, 2000
 
+    it 'converts 5 to V' do
+      expect(convert_to_roman(5)).to eq "V"
+    end
+
+    it 'converts 9 to VIIII' do
+      expect(convert_to_roman(9)).to eq "VIIII"
+    end
+
+    it 'converts 10 to X' do
+      expect(convert_to_roman(10)).to eq "X"
+    end
+
+    it 'converts 18 to XVIII' do
+      expect(convert_to_roman(18)).to eq "XVIII"
+    end
+
+    it 'converts 55 to LV' do
+      expect(convert_to_roman(55)).to eq "LV"
+    end
+
+    it 'converts 101 to CI' do
+      expect(convert_to_roman(101)).to eq "CI"
+    end
+
+    it 'converts 600 to DC' do
+      expect(convert_to_roman(600)).to eq "DC"
+    end
+
+    it 'converts 2000 to MM' do
+      expect(convert_to_roman(2000)).to eq "MM"
+    end
   end
 
   describe 'modern Roman numerals' do
