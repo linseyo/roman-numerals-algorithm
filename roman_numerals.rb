5b87df33d866f9150212134fb9fff8b4d6d749d5
Eitan Greene & Matthew Fischer <pair+eitan-g.mattgfisch@devbootcamp.com>
2017-03-06 11:48:32 -0800

    Refactors each loop to use each_with_index instead

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -3,12 +3,11 @@ def convert_to_roman(arabic_number)
   numbers = [1000, 500, 100, 50, 10, 5, 1]
 
   response = ""
-  numbers.each do |num|
+  numbers.each_with_index do |num, index|
     if arabic_number >= num
-      response += letters[numbers.index(num)] * (arabic_number / num)
+      response += letters[index] * (arabic_number / num)
       arabic_number = arabic_number % num
     end
   end
-
   response
 end
