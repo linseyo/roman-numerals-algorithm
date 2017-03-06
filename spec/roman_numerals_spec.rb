fb426b4772cc2f6ca4a92c37a5e6c022ecd94246
Jordan Levine & Julia (Julie) Xu <pair+j0rdania.jmxu94@devbootcamp.com>
2017-03-06 15:42:22 -0800

    Add convert5 rspec test

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -13,6 +13,9 @@ describe 'converting an Arabic number to a Roman numeral' do
     # Release 1 ...
     # add tests for old roman numerals here
 
+    it 'converts 5 to V' do
+      expect(convert_to_roman(5)).to eq "V"
+    end
   end
 
   describe 'modern Roman numerals' do
