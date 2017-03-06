715a7ec6f24a2a1e023ef3f0543d9b8fcb193ee8
Jordan Levine & Julia (Julie) Xu <pair+j0rdania.jmxu94@devbootcamp.com>
2017-03-06 15:53:38 -0800

    Add test cases for several numbers for V and I old roman numeral conversion method

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -16,6 +16,30 @@ describe 'converting an Arabic number to a Roman numeral' do
     it 'converts 5 to V' do
       expect(convert_to_roman(5)).to eq "V"
     end
+
+    it 'converts 6 to VI' do
+      expect(convert_to_roman(6)).to eq "VI"
+    end
+
+    it 'converts 7 to VII' do
+      expect(convert_to_roman(7)).to eq "VII"
+    end
+
+    it 'converts 8 to VIII' do
+      expect(convert_to_roman(8)).to eq "VIII"
+    end
+
+    it 'converts 13 to VVIII' do
+      expect(convert_to_roman(13)).to eq "VVIII"
+    end
+
+    it 'converts 15 to VVV' do
+      expect(convert_to_roman(15)).to eq "VVV"
+    end
+
+    it 'converts 16 to VVVI' do
+      expect(convert_to_roman(16)).to eq "VVVI"
+    end
   end
 
   describe 'modern Roman numerals' do
