bf3e6c85ece588e01feb8e0ac3422aa0801c9d6e
Eitan Greene & Matthew Fischer <pair+eitan-g.mattgfisch@devbootcamp.com>
2017-03-06 11:37:07 -0800

    Converts all numbers to old roman

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,3 +1,34 @@
 def convert_to_roman(arabic_number)
-  "I"*arabic_number
+  letters = ["M", "D", "C", "L"]
+  response = ""
+  if arabic_number >= 1000
+    response += "M" * (arabic_number / 1000)
+    arabic_number = arabic_number % 1000
+  end
+  if arabic_number >= 500
+    response += "D" * (arabic_number / 500)
+    arabic_number = arabic_number % 500
+  end
+  if arabic_number >= 100
+    response += "C" * (arabic_number / 100)
+    arabic_number = arabic_number % 100
+  end
+  if arabic_number >= 50
+    response += "L" * (arabic_number / 50)
+    arabic_number = arabic_number % 50
+  end
+  if arabic_number >= 10
+    response += "X" * (arabic_number / 10)
+    arabic_number = arabic_number % 10
+  end
+  if arabic_number >= 5
+    response += "V" * (arabic_number / 5)
+    arabic_number = arabic_number % 5
+  end
+  if arabic_number >= 1
+    response += "I" * (arabic_number / 1)
+    arabic_number = arabic_number % 1
+  end
+
+  response
 end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -12,7 +12,33 @@ describe 'converting an Arabic number to a Roman numeral' do
 
     # Release 1 ...
     # add tests for old roman numerals here
+    it 'converts 5 to V' do
+      expect(convert_to_roman(5)).to eq "V"
+    end
 
+    it 'converts 6 to VI' do
+      expect(convert_to_roman(6)).to eq "VI"
+    end
+
+    it 'converts 16 to XVI' do
+      expect(convert_to_roman(16)).to eq "XVI"
+    end
+
+    it 'converts 66 to LXVI' do
+      expect(convert_to_roman(66)).to eq "LXVI"
+    end
+
+    it 'converts 166 to CLXVI' do
+      expect(convert_to_roman(166)).to eq "CLXVI"
+    end
+
+    it 'converts 666 to DCLXVI' do
+      expect(convert_to_roman(666)).to eq "DCLXVI"
+    end
+
+    it 'converts 1666 to MDCLXVI' do
+      expect(convert_to_roman(1666)).to eq "MDCLXVI"
+    end
   end
 
   describe 'modern Roman numerals' do
