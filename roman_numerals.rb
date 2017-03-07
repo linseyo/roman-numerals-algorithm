c8584f247f30f834b7114690d8e9668702d7cce2
Austin Kading <email": null>
2017-03-06 17:20:04 -0800

    converts 1 and 4 to its roman numeral

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,3 +1,11 @@
 def convert_to_roman(arabic_number)
-  # Your code goes here
+  # Converts arabic number into "I"
+  arabic_array = [1, 4]
+  roman_array = ["I", "IIII"]
+
+  # Converts arabic 1 to "I"
+  # arabic_number = arabic_array[0] = roman_array[0]
+  arabic_index = arabic_array.find_index(arabic_number)
+  roman_string = roman_array[arabic_index]
+
 end
