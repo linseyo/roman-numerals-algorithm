6fac6faa05898cb634189ac66261cb30f219173e
Joshua Mun & Seth Gerou <pair+joshmun.sethgerou@devbootcamp.com>
2017-03-06 16:52:33 -0800

    Add modern roman metbhods, and tests...

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,3 +1,78 @@
-def convert_to_roman(arabic_number)
-  # Your code goes here
+
+
+# def convert_to_roman(arabic_number)
+#    = arabic_number.to_s.length
+#   quotient = arabic_number
+#   remainder = arabic_number%
+#   "I" * remainder
+# end
+def convert_to_roman(arabic_number, modern = false)
+  conversion_table = [
+  {1=>"I", 4=>"IV", 5=>"V", 9=>"IX"},
+  {1=>"X", 4=>"XL", 5=>"L", 9=>"XC"},
+  {1=>"C", 4=>"CD", 5=>"D", 9=>"CM"},
+  {1=>"M"}
+]
+  result = ""
+  digits = arabic_number.to_s.split("")
+  digits.each.with_index do |digit, index|
+    if digit.to_i < 5
+      result += mroman(((conversion_table[(digits.length - index)-1][1]) * digit.to_i), modern)
+    elsif digit.to_i == 5
+      result += mroman((conversion_table[(digits.length - index)-1][5]), modern)
+    else
+      result += mroman(((conversion_table[(digits.length - index)-1][5]) +
+        ((conversion_table[(digits.length - index)-1][1]) * (digit.to_i%5))), modern)
+    end
+  end
+  result
 end
+
+# Pseudocode
+def mroman(old_roman, modern)
+  if modern == true
+    case old_roman
+      when "IIII"
+      old_roman = "IV"
+      when "VIIII"
+      old_roman = "IX"
+      when "XXXX"
+      old_roman = "XL"
+      when "LXXXX"
+      old_roman = "XC"
+      when "CCCC"
+      old_roman = "CD"
+      when "DCCCC"
+      old_roman = "CM"
+    end
+  end
+old_roman
+end
+
+
+
+# #def check_five(arabic_number)
+#   case arabic_number
+#   when arabic_number<5
+#   when 5
+#   when 5<arabic_number && arabic_number<10
+#   when 10
+#   when 10<arabic_number && arabic_number<50
+#   when 50
+#   when 50<arabic_number && arabic_number<100
+#   when 100
+#   when 100<arabic_number && arabic_number<500
+#   when 500
+#   when 500<arabic_number && arabic_number<1000
+#   when 1000
+#   when 1000<arabic_number
+#   end
+# #end
+# Driver Code
+# p convert(137, conversion_table)
+# p convert(1092, conversion_table)
+# p convert(9, conversion_table)
+# p convert(555, conversion_table)
+p convert_to_roman(4, true)
+p convert_to_roman(99, true)
+p convert_to_roman(99)
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -13,6 +13,34 @@ describe 'converting an Arabic number to a Roman numeral' do
     # Release 1 ...
     # add tests for old roman numerals here
 
+    it 'converts 5 to V' do
+      expect(convert_to_roman(5)).to eq "V"
+    end
+
+    it 'converts 6 to VI' do
+      expect(convert_to_roman(6)).to eq "VI"
+    end
+
+    it 'converts 10 to X' do
+      expect(convert_to_roman(10)).to eq "X"
+    end
+
+    it 'modern true converts 4 to IV' do
+      expect(convert_to_roman(4, true)).to eq "IV"
+    end
+
+    it 'modern true converts 99 to XCIX' do
+      expect(convert_to_roman(99, true)).to eq "XCIX"
+    end
+
+    it 'modern true converts 1 to I' do
+      expect(convert_to_roman(1, true)).to eq "I"
+    end
+
+    it 'modern true converts 999 to CMXCIX' do
+      expect(convert_to_roman(999, true)).to eq "CMXCIX"
+    end
+
   end
 
   describe 'modern Roman numerals' do
