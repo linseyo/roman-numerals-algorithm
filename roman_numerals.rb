c0c613cf679786f9cda92e80e8233718714b2540
Eitan Greene <email": null>
2017-03-12 21:56:08 -0700

    Refactors 'if not modern' clause

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -7,66 +7,38 @@
 # end
 
 def convert_to_roman(arabic_number, modern = false)
-  arabic_string = arabic_number.to_s
-    arabic_array = arabic_string.split("")
-    roman_array = ""
-  if modern == false
+  arabic_array = arabic_number.to_s.split("")
+  roman_array = ""
+  if !modern
+    potential_lengths = [3,2,1]
+    letters = ["D", "C", "L", "X", "V", "I"]
+    counter = 0
     #Thousands column:
     if arabic_array.length == 4
-      if arabic_array[0] == "3"
-        roman_array += "MMM"
-      elsif arabic_array[0] == "2"
-        roman_array += "MM"
-      else
-        roman_array += "M"
-      end
+      roman_array += "M" * arabic_array[0].to_i
       arabic_array.delete_at(0)
     end
     #Hundreds column:
-    if  arabic_array.length == 3
-      hundreds_digit = arabic_array[0]
-      if hundreds_digit.to_i.between?(5,9)
-        roman_array += "D"
-        number_of_cs = hundreds_digit.to_i - 5
-        roman_array += "C" * number_of_cs.to_i
-      else
-        roman_array += "C" * hundreds_digit.to_i
-      end
+    potential_lengths.each do |potential_length|
+      if arabic_array.length == potential_length
+        first_digit = arabic_array[0].to_i
+        equivalent_arabic = letters[counter]
+        next_arabic = letters[counter + 1]
+        # if first_digit.between?(5,9)
+          roman_array += next_arabic * first_digit
+        # end
       arabic_array.delete_at(0)
-    end
-    #Tens column:
-    if arabic_array.length == 2
-      tens_digit = arabic_array[0]
-      if tens_digit.to_i.between?(5,9)
-        roman_array += "L"
-        number_of_ls = tens_digit.to_i - 5
-        roman_array += "X" * number_of_ls.to_i
-      else
-        roman_array += "X" * tens_digit.to_i
       end
-      arabic_array.delete_at(0)
+      counter += 2
     end
-    if arabic_array.length == 1
-      ones_digit = arabic_array[0]
-      if ones_digit.to_i.between?(5,9)
-        roman_array += "V"
-        number_of_vs = ones_digit.to_i - 5
-        roman_array += "I" * number_of_vs.to_i
-      else
-        roman_array += "I" * ones_digit.to_i
-      end
-    end
-  elsif modern == true
+
+  else
+    potential_lengths = [3,2,1]
+    letters = ["D", "C", "L", "X", "V", "I"]
+    counter = 0
     #thousands column
-    p arabic_array
     if arabic_array.length == 4
-      if arabic_array[0] == "3"
-        roman_array += "MMM"
-      elsif arabic_array[0] == "2"
-        roman_array += "MM"
-      else
-        roman_array += "M"
-      end
+      roman_array += "M" * arabic_array[0].to_i
       arabic_array.delete_at(0)
     end
     #Hundreds column:
@@ -116,23 +88,7 @@ def convert_to_roman(arabic_number, modern = false)
       end
     end
   end
-  ## Print out array as a string
-  p roman_array
+  roman_array
 end
-
-
-convert_to_roman(1143, true)
-convert_to_roman(2974, true)
-
-
-# Release 2 Psuedocode
-# We receive a 4 digit number
-# Inspect the first digit ....
-# Inspect the second digit
-  # Already have 1-4 do this...
-  # 5-9 do this...
-
-
-
 
 
