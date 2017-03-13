b768646e17986b9615bce2219f6efa2ab356a62c
Dan McKeon <dpmckeon@gmail.com>
2017-03-12 23:27:07 -0500

    Add logic for calculating numbers greater than 5

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,7 +1,22 @@
 def convert_to_roman(arabic_number, modern = false)
+  arabic_to_roman = {1 => "I", 5 => "V" }
   roman_numeral = ""
-  arabic_number.times do
+
+  num_fives = arabic_number / 5
+
+
+  num_fives.times do
+    roman_numeral += "V"
+  end
+
+  remainder = arabic_number - (num_fives * 5)
+  
+
+
+  remainder.times do
     roman_numeral += "I"
   end
   roman_numeral
+
+
 end
