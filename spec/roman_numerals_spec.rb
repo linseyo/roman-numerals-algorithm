d2e2d666c31505723616f827e0cb9d26cc41a25b
Dan McKeon <dpmckeon@gmail.com>
2017-03-12 23:20:02 -0500

    wipe existing roman numeral challenge code to start anew and refactor testse

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../roman_numerals'
 
 describe 'converting an Arabic number to a Roman numeral' do
-  describe 'old Roman numerals' do
+  context 'modern false' do
     it 'converts 1 to I' do
       expect(convert_to_roman(1)).to eq "I"
     end
@@ -9,9 +9,6 @@ describe 'converting an Arabic number to a Roman numeral' do
     it 'converts 4 to IIII' do
       expect(convert_to_roman(4)).to eq "IIII"
     end
-
-    # Release 1 ...
-    # add tests for old roman numerals here
 
     it 'converts 5 to V' do
       expect(convert_to_roman(5)).to eq "V"
@@ -24,28 +21,27 @@ describe 'converting an Arabic number to a Roman numeral' do
     it 'converts 10 to X' do
       expect(convert_to_roman(10)).to eq "X"
     end
+  end
 
-    it 'modern true converts 4 to IV' do
-      expect(convert_to_roman(4, true)).to eq "IV"
-    end
-
-    it 'modern true converts 99 to XCIX' do
-      expect(convert_to_roman(99, true)).to eq "XCIX"
-    end
-
-    it 'modern true converts 1 to I' do
+  context 'modern true' do
+    it 'converts 1 to I' do
       expect(convert_to_roman(1, true)).to eq "I"
     end
 
-    it 'modern true converts 999 to CMXCIX' do
-      expect(convert_to_roman(999, true)).to eq "CMXCIX"
+    it 'converts 4 to IV' do
+      expect(convert_to_roman(4, true)).to eq "IV"
     end
 
-  end
+    it 'converts 9 to IX' do
+      expect(convert_to_roman(9, true)).to eq "IX"
+    end    
 
-  describe 'modern Roman numerals' do
-    # Release 3 ...
-    # add tests for modern roman numerals here
+    it 'converts 99 to XCIX' do
+      expect(convert_to_roman(99, true)).to eq "XCIX"
+    end
 
+    it 'converts 999 to CMXCIX' do
+      expect(convert_to_roman(999, true)).to eq "CMXCIX"
+    end
   end
 end
