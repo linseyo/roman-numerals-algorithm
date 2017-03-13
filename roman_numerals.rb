5cd8d4b139dfc890f0454e190e07f0904e361130
Dan McKeon <dpmckeon@gmail.com>
2017-03-12 23:38:36 -0500

    add logic to convert all numerals non-modern style

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -6,23 +6,34 @@ def convert_to_roman(arabic_number, modern = false)
                       100 => "C",
                       500 => "D",
                       1000 => "M"}
+  
+  conversion_order = [1000, 500, 100, 50, 10, 5, 1]
   roman_numeral = ""
+  remainder = arabic_number
 
-  num_fives = arabic_number / 5
-
-
-  num_fives.times do
-    roman_numeral += "V"
+  conversion_order.each do |number|
+    numerals = remainder / number
+    numerals.times { roman_numeral << arabic_to_roman[number] }
+    remainder -= numerals * number
   end
 
-  remainder = arabic_number - (num_fives * 5)
+  roman_numeral
+
+  # num_fives = arabic_number / 5
+
+
+  # num_fives.times do
+  #   roman_numeral += "V"
+  # end
+
+  # remainder = arabic_number - (num_fives * 5)
   
 
 
-  remainder.times do
-    roman_numeral += "I"
-  end
-  roman_numeral
+  # remainder.times do
+  #   roman_numeral += "I"
+  # end
+  # roman_numeral
 
 
 end
