1e03158d3ee90a5f81c97dda5cfb4bf97ea590fa
Dan McKeon <dpmckeon@gmail.com>
2017-03-12 23:44:26 -0500

    add logic for modern conversion

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,13 +1,24 @@
 def convert_to_roman(arabic_number, modern = false)
-  arabic_to_roman =   {1 => "I", 
-                      5 => "V", 
-                      10 => "X",
-                      50 => "L",
-                      100 => "C",
-                      500 => "D",
-                      1000 => "M"}
-  
-  conversion_order = [1000, 500, 100, 50, 10, 5, 1]
+  arabic_to_roman =           {1 => "I", 
+                              5 => "V", 
+                              10 => "X",
+                              50 => "L",
+                              100 => "C",
+                              500 => "D",
+                              1000 => "M"}
+  if modern
+    modern_roman_additions =  {4 => "IV",
+                              9 => "IX",
+                              40 => "XL",
+                              90 => "XC",
+                              400 => "CD",
+                              900 => "CM"}
+    arabic_to_roman.merge!(modern_roman_additions)
+    conversion_order = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+  else
+    conversion_order = [1000, 500, 100, 50, 10, 5, 1]
+  end
+
   roman_numeral = ""
   remainder = arabic_number
 
@@ -18,22 +29,4 @@ def convert_to_roman(arabic_number, modern = false)
   end
 
   roman_numeral
-
-  # num_fives = arabic_number / 5
-
-
-  # num_fives.times do
-  #   roman_numeral += "V"
-  # end
-
-  # remainder = arabic_number - (num_fives * 5)
-  
-
-
-  # remainder.times do
-  #   roman_numeral += "I"
-  # end
-  # roman_numeral
-
-
 end
